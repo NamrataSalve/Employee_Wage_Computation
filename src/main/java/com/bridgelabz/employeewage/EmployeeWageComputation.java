26759373eed49435c560fa46cde1c02fdc29dcd5
NamrataSalve <namratasalve1999@gmail.com>
2024-09-25 12:15:27 +0530

    Welcome Employee Wage Computation Problem

---
+++ src/main/java/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employeewage;
+
+public class EmployeeWageComputation
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Employee Wage Computation Problem" );
+    }
+}
